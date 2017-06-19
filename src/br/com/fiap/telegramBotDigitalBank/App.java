79119635367c43001b6cd4ce92b71b3eb192135d
DiigoO <rodrigo_p_souza@hotmail.com>
2017-06-19 02:46:42 -0300

    first commit

---
+++ src/br/com/fiap/telegramBotDigitalBank/App.java
@@ -0,0 +1,13 @@
+package br.com.fiap.telegramBotDigitalBank;
+
+public class App {
+
+	public static void main(String[] args) {
+		/**
+		 * http://michelpf.com/blog/2016/10/03/criando-bots-do-telegram-com-api-java/
+		 * https://core.telegram.org/bots
+		 * https://github.com/pengrad/java-telegram-bot-api
+		 */
+	}
+
+}
---
+++ src/br/com/fiap/twitterSmallAnalytics/App.java
@@ -0,0 +1,12 @@
+package br.com.fiap.twitterSmallAnalytics;
+
+public class App {
+
+	public static void main(String[] args) {
+		/**
+		 * http://twitter4j.org/en/index.html
+		 * https://github.com/yusuke/twitter4j
+		 */
+	}
+
+}
